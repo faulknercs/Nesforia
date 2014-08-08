8277496c827c64605d31ad7f7cbfbfe3480abbc2
Artem Stepanyuk <faulknercs@yandex.ru>
2014-08-08 09:47:49 +0300

    Added access methods to APU interface

--- Sources/Nesforia.Core/Apu/IApu.cs
+++ Sources/Nesforia.Core/Apu/IApu.cs
@@ -24,8 +24,138 @@
 #endregion
 namespace Nesforia.Core.Apu
 {
+    /// <summary>
+    /// Interface of Audio Processing Unit in the NES console, which is responsible for generating sound for games.
+    /// </summary>
+    /// <remarks>
+    /// Detailed description: http://wiki.nesdev.com/w/index.php/APU
+    /// </remarks>
     public interface IApu
     {
-         
+        /// <summary>
+        /// Duty (D), envelope loop / length counter halt (L), constant volume (C), volume/envelope (V) for square wave 1
+        /// </summary>
+        /// <param name="value">DDLC NNNN</param>
+        void Write4000(byte value);
+
+        /// <summary>
+        /// Sweep unit for square wave 1: enabled (E), period (P), negate (N), shift (S) 
+        /// </summary>
+        /// <param name="value">EPPP NSSS</param>
+        void Write4001(byte value);
+
+        /// <summary>
+        /// Low byte of period for square wave 1 
+        /// </summary>
+        /// <param name="value">Value of low byte</param>
+        void Write4002(byte value);
+
+        /// <summary>
+        /// High byte of period (T) and length counter (L) value for square wave 1
+        /// </summary>
+        /// <param name="value">LLLL LTTT</param>
+        void Write4003(byte value);
+
+        /// <summary>
+        /// Duty (D), envelope loop / length counter halt (L), constant volume (C), volume/envelope (V) for square wave 2
+        /// </summary>
+        /// <param name="value">DDLC NNNN</param>
+        void Write4004(byte value);
+
+        /// <summary>
+        /// Sweep unit for square wave 2: enabled (E), period (P), negate (N), shift (S) 
+        /// </summary>
+        /// <param name="value">EPPP NSSS</param>
+        void Write4005(byte value);
+
+        /// <summary>
+        /// Low byte of period for square wave 2
+        /// </summary>
+        /// <param name="value">Value of low byte</param>
+        void Write4006(byte value);
+
+        /// <summary>
+        /// High byte of period (T) and length counter (L) value for square wave 2
+        /// </summary>
+        /// <param name="value">LLLL LTTT</param>
+        void Write4007(byte value);
+
+        /// <summary>
+        /// Length counter halt / linear counter control (C), linear counter load (R) for triangle wave
+        /// </summary>
+        /// <param name="value">CRRR RRRR</param>
+        void Write4008(byte value);
+
+        /// <summary>
+        /// Low byte of period for triangle wave
+        /// </summary>
+        /// <param name="value">Value of low byte</param>
+        void Write400A(byte value);
+
+        /// <summary>
+        /// High byte of period (T) and length counter (L) value for triangle wave
+        /// </summary>
+        /// <param name="value">LLLL LTTT</param>
+        void Write400B(byte value);
+
+        /// <summary>
+        /// Envelope loop / length counter halt (L), constant volume (C), volume/envelope (V) for noise generator
+        /// </summary>
+        /// <param name="value">--LC VVVV</param>
+        void Write400C(byte value);
+
+        /// <summary>
+        /// Loop noise (L), noise period (P) for noise generator
+        /// </summary>
+        /// <param name="value">L--- PPPP</param>
+        void Write400E(byte value);
+
+        /// <summary>
+        /// Length counter load (L) for noise generator
+        /// </summary>
+        /// <param name="value">LLLL L---</param>
+        void Write400F(byte value);
+
+        /// <summary>
+        /// IRQ enable (I), loop (L), frequency (R) for DMC channel
+        /// </summary>
+        /// <param name="value">IL-- RRRR</param>
+        void Write4010(byte value);
+
+        /// <summary>
+        /// Load counter (D) for DMC channel
+        /// </summary>
+        /// <param name="value">-DDD DDDD</param>
+        void Write4011(byte value);
+
+        /// <summary>
+        /// Writes start of DMC waveform (at address $C000 + $40*$xx)
+        /// </summary>
+        /// <param name="value">Part of adress (11xxxxxx.xx000000)</param>
+        void Write4012(byte value);
+
+        /// <summary>
+        /// Writes length of DMC waveform.
+        /// </summary>
+        /// <param name="value">Length (0000LLLL.LLLL0001)</param>
+        void Write4013(byte value);
+
+        /// <summary>
+        /// Writes enable chanels flags (Enable DMC (D), noise (N), triangle (T), and pulse channels (2/1))
+        /// </summary>
+        /// <param name="value">---D NT21</param>
+        void Write4015(byte value);
+
+        /// <summary>
+        /// Reads status of channels (DMC interrupt (I), frame interrupt (F), DMC active (D), length counter > 0 (N/T/2/1))
+        /// </summary>
+        /// <returns>IF-D NT21</returns>
+        byte Read4015();
+
+        /// <summary>
+        /// Frame counter control (Mode (M, 0 = 4-step, 1 = 5-step), IRQ inhibit flag (I))
+        /// </summary>
+        /// <param name="value">MI-- ----</param>
+        void Write4017(byte value);
     }
 }
--- Sources/Nesforia.Interpreter/Apu/Apu.cs
+++ Sources/Nesforia.Interpreter/Apu/Apu.cs
@@ -28,6 +28,109 @@ namespace Nesforia.Interpreter.Apu
 {
     public class Apu : IApu 
     {
-         
+        public void Write4000(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write4001(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write4002(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write4003(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write4004(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write4005(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write4006(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write4007(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write4008(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write400A(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write400B(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write400C(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write400E(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write400F(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write4010(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write4011(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write4012(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write4013(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write4015(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public byte Read4015()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write4017(byte value)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
